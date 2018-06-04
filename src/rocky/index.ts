817bcf3474a1c4f2eee836225d930271b636d14d
Ohle Claussen <joclaussen@gmail.com>
2018-06-04 23:10:17 +0200

    Proof of concept drawing

--- src/rocky/index.ts
+++ src/rocky/index.ts
@@ -1,2 +1,9 @@
 import rocky = require('rocky')
 
+rocky.on("draw", (evt : rocky.DrawEvent) => {
+    var ctx = evt.context;
+    ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("abc", 0, 0);
+});
