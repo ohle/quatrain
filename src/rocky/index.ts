7484d7741b75a8fd234bf55c8a0effdfee112bfc
Ohle Claussen <joclaussen@gmail.com>
2018-06-15 22:57:26 +0200

    Pre-initialize complications

--- src/rocky/index.ts
+++ src/rocky/index.ts
@@ -7,13 +7,20 @@ var complications: Complication[] = [
     new DigitalTime("26px bold Leco-numbers-am-pm", "white")
 ];
 
+var initialized = false;
+
+var initialize = (ctx : RockyCanvasRenderingContext2D) => {
+    var dim = new geom.Dimension(ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    complications.forEach(c => c.init(dim));
+    initialized = true;
+}
+
 rocky.on("draw", (evt : rocky.DrawEvent) => {
     var ctx = evt.context;
     ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-    var dim = new geom.Dimension(ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    initialize(ctx);
     for (let complication of complications) {
         ctx.save();
-        complication.init(dim);
         complication.draw(ctx);
         ctx.restore();
     }
