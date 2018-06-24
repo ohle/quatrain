924ce353341f2854047070bbe9dad5b592701507
Ohle Claussen <joclaussen@gmail.com>
2018-06-24 22:47:50 +0200

    data is non-optional

--- src/rocky/watchface.ts
+++ src/rocky/watchface.ts
@@ -3,7 +3,7 @@ import geom = require('./geometry')
 
 export default interface Complication<PHONEMESSAGE> {
     init(screenSize : geom.Dimension) : void;
-    draw(ctx : RockyCanvasRenderingContext2D, data? : PHONEMESSAGE);
+    draw(ctx : RockyCanvasRenderingContext2D, data : PHONEMESSAGE);
     phoneMessageKey? : string;
 }
 
